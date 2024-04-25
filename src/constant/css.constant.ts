04c69778b9058b3ac19b0b71916a191e62cb33cc
Fizzbow <55476631+Fizzbow@users.noreply.github.com>
2024-04-25 20:56:04 +0800

    feat:add global constants

--- src/constant/css.constant.ts
+++ src/constant/css.constant.ts
@@ -1,16 +1,49 @@
 // import { ThemeColor } from "./themes.constant";
 
-import { CSSStatus } from "../types";
+import { CSSStatus, ThemeColor, ThemeVariable } from "../types";
 
 export const GLOBAL_VAR_STATUS: Record<CSSStatus, string> = {
   error: "248,113,113",
 };
 
-// export const globalVariableTheme: Record<ThemeColor, themeVariable> = {
-//     green: {
-//         primary: "1,2,10",
-//     },
-//     purple: undefined,
-//     dark: undefined,
-//     pinky: undefined
-// };
+export const globalVariableTheme: Record<
+  ThemeColor,
+  Record<ThemeVariable, string>
+> = {
+  green: {
+    primary: "70, 131, 77",
+    secondary: "250, 255, 192",
+    "tint-1": "255, 255, 255",
+    "tint-2": "188, 188, 188",
+    "tint-3": "78, 78, 78",
+    checkedOutline: "171, 206, 175",
+    check: "70, 131, 77",
+  },
+  purple: {
+    primary: "143, 109, 214",
+    secondary: "238, 232, 216",
+    "tint-1": "255, 255, 255",
+    "tint-2": "188, 188, 188",
+    "tint-3": "78, 78, 78",
+    checkedOutline: "215, 210, 216",
+    check: "143, 109, 214",
+  },
+  dark: {
+    primary: "35, 50, 90",
+    secondary: "230, 166, 113",
+    "tint-1": "255, 255, 255",
+    "tint-2": "188, 188, 188",
+    "tint-3": "78, 78, 78",
+    checkedOutline: "239, 224, 212",
+    check: " 230, 166, 113",
+  },
+  pinky: {
+    primary: "248, 237, 217",
+    secondary: "70, 131, 77",
+    "tint-1": "255, 255, 255",
+    "tint-2": "188, 188, 188",
+    "tint-3": "78, 78, 78",
+    checkedOutline: "171, 206, 175",
+    check: "70, 131, 77",
+  },
+};
--- src/types/variable.type.ts
+++ src/types/variable.type.ts
@@ -1,4 +1,13 @@
 export type ThemeColor = "green" | "purple" | "dark" | "pinky";
+export type ThemeVariable =
+  | "primary"
+  | "secondary"
+  | "tint-1"
+  | "tint-2"
+  | "tint-3"
+  | "checkedOutline"
+  | "check";
+
 export interface Theme {
   themeId: ThemeColor;
   color: string;
